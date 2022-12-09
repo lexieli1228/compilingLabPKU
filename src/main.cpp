39434e5b670da74d43fa8d2f29ee8bef3fd0c3c9
lexieli1228 <84088099+lexieli1228@users.noreply.github.com>
2022-12-09 18:48:57 +0800

    Add: visit functions for riscv output generation
    lv2 implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "koopa.h"
 #include <cassert>
 #include <cstdio>
 #include <iostream>
@@ -10,6 +11,124 @@ using namespace std;
 extern FILE *yyin;
 extern FILE *yyout;
 extern int yyparse(unique_ptr<BaseAST> &ast);
+
+void Visit(const koopa_raw_program_t &program);
+void Visit(const koopa_raw_slice_t &slice);
+void Visit(const koopa_raw_function_t &func);
+void Visit(const koopa_raw_basic_block_t &bb);
+void Visit(const koopa_raw_value_t &value);
+void Visit(const koopa_raw_return_t &ret);
+void Visit(const koopa_raw_integer_t &integer);
+
+/*  
+  .text
+  .globl main
+main:
+  li a0, 0
+  ret
+*/
+
+// 访问 raw program
+void Visit(const koopa_raw_program_t &program) {
+  // 执行一些其他的必要操作
+  // 使用 for 循环遍历函数列表
+  cout << " .text\n";
+
+  cout << " .globl ";
+  for (size_t i = 0; i < program.funcs.len; ++i) {
+    // 正常情况下, 列表中的元素就是函数, 我们只不过是在确认这个事实
+    // 当然, 你也可以基于 raw slice 的 kind, 实现一个通用的处理函数
+    assert(program.funcs.kind == KOOPA_RSIK_FUNCTION);
+    // 获取当前函数
+    koopa_raw_function_t func = (koopa_raw_function_t) program.funcs.buffer[i];
+    // 进一步处理当前函数
+    // ...
+    string tempFuncName = func->name;
+    tempFuncName.erase(0, 1);
+    cout << tempFuncName << "\n";
+  }
+
+  // 访问所有全局变量
+  Visit(program.values);
+  // 访问所有函数
+  Visit(program.funcs);
+}
+
+// 访问 raw slice
+void Visit(const koopa_raw_slice_t &slice) {
+  for (size_t i = 0; i < slice.len; ++i) {
+    auto ptr = slice.buffer[i];
+    // 根据 slice 的 kind 决定将 ptr 视作何种元素
+    switch (slice.kind) {
+      case KOOPA_RSIK_FUNCTION:
+        // 访问函数
+        Visit(reinterpret_cast<koopa_raw_function_t>(ptr));
+        break;
+      case KOOPA_RSIK_BASIC_BLOCK:
+        // 访问基本块
+        Visit(reinterpret_cast<koopa_raw_basic_block_t>(ptr));
+        break;
+      case KOOPA_RSIK_VALUE:
+        // 访问指令
+        Visit(reinterpret_cast<koopa_raw_value_t>(ptr));
+        break;
+      default:
+        // 我们暂时不会遇到其他内容, 于是不对其做任何处理
+        assert(false);
+    }
+  }
+}
+
+// 访问函数
+void Visit(const koopa_raw_function_t &func) {
+  // 执行一些其他的必要操作
+  string tempFuncName = func->name;
+  tempFuncName.erase(0, 1);
+  cout << tempFuncName << ":\n";
+  // 访问所有基本块
+  Visit(func->bbs);
+}
+
+// 访问基本块
+void Visit(const koopa_raw_basic_block_t &bb) {
+  // 执行一些其他的必要操作
+  // ...
+  // 访问所有指令
+  Visit(bb->insts);
+}
+
+// 访问指令
+void Visit(const koopa_raw_value_t &value) {
+  // 根据指令类型判断后续需要如何访问
+  const auto &kind = value->kind;
+  switch (kind.tag) {
+    case KOOPA_RVT_RETURN:
+      // 访问 return 指令
+      Visit(kind.data.ret);
+      break;
+    case KOOPA_RVT_INTEGER:
+      // 访问 integer 指令
+      Visit(kind.data.integer);
+      break;
+    default:
+      // 其他类型暂时遇不到
+      assert(false);
+  }
+}
+
+// 访问ret
+void Visit(const koopa_raw_return_t &ret)
+{
+  // cout << " li a0, " << ret.value->kind.data.integer.value << "\n";
+  cout << " li a0, " << ret.value->kind.data.integer.value << "\n";
+  cout << " ret\n";
+}
+
+// 访问integer
+void Visit(const koopa_raw_integer_t &integer)
+{
+  cout << integer.value << "\n";
+}
 
 int main(int argc, const char *argv[])
 {
@@ -27,12 +146,44 @@ int main(int argc, const char *argv[])
 
   // parse input file
   unique_ptr<BaseAST> ast;
-  auto ret = yyparse(ast);
-  assert(!ret);
+  auto ret0 = yyparse(ast);
+  assert(!ret0);
 
-  // dump AST
-  ast->Dump();
-  cout << endl;
+  // original koopa program
+  string strOrigin = "";
+
+  // dump AST into string
+  ast->Dump(strOrigin);
+
+  // 转化成内存形式
+  // 解析字符串 str, 得到 Koopa IR 程序
+  koopa_program_t program;
+  koopa_error_code_t ret = koopa_parse_from_string(strOrigin.c_str(), &program);
+  assert(ret == KOOPA_EC_SUCCESS);  // 确保解析时没有出错
+  // 创建一个 raw program builder, 用来构建 raw program
+  koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+  // 将 Koopa IR 程序转换为 raw program
+  koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+  // 释放 Koopa IR 程序占用的内存
+  koopa_delete_program(program);
+  
+  // get output
+  // lv1, directly output koopa program
+  if (mode[1] == 'k')
+  {
+    cout << strOrigin;
+  }
+  // lv2, generate riscv output
+  // 处理 raw program
+  else if (mode[1] == 'r') 
+  {
+    Visit(raw);
+  }
+
+  // 处理完成, 释放 raw program builder 占用的内存
+  // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
+  // 所以不要在 raw program 处理完毕之前释放 builder
+  koopa_delete_raw_program_builder(builder);
 
   return 0;
 }
