420f5ab8c7ca02704d1115c67bdeef4cfc114adf
lexieli1228 <84088099+lexieli1228@users.noreply.github.com>
2022-12-11 00:35:11 +0800

    Add: AST implementation for lv3.1, basic one-operand calculation
    ast components construction, dump function modification, adding functions for returning assigned registers and numbers, generate output via modifying  a given pointer

--- src/AST.hpp
+++ src/AST.hpp
@@ -5,12 +5,20 @@
 #include <memory>
 #include <string>
 
+extern int registerCnt;
+
 class BaseAST
 {
 public:
     virtual ~BaseAST() = default;
 
-    virtual void Dump(std::string& strOriginal) const = 0;
+    virtual void Dump(std::string &strOriginal) const = 0;
+
+    // 返回寄存器的编号或者一个数值
+    virtual std::string ReversalDump(std::string &strOriginal)
+    {
+        return "";
+    }
 };
 
 class CompUnitAST : public BaseAST
@@ -18,7 +26,7 @@ class CompUnitAST : public BaseAST
 public:
     std::unique_ptr<BaseAST> func_def;
 
-    void Dump(std::string& strOriginal) const override
+    void Dump(std::string &strOriginal) const override
     {
         func_def->Dump(strOriginal);
     }
@@ -31,7 +39,7 @@ public:
     std::string ident;
     std::unique_ptr<BaseAST> block;
 
-    void Dump(std::string& strOriginal) const override
+    void Dump(std::string &strOriginal) const override
     {
         strOriginal += "fun @";
         strOriginal += ident;
@@ -48,7 +56,7 @@ class FuncTypeAST : public BaseAST
 {
 public:
     std::string funcType;
-    void Dump(std::string& strOriginal) const override
+    void Dump(std::string &strOriginal) const override
     {
         if (funcType == "int")
         {
@@ -61,22 +69,157 @@ class BlockAST : public BaseAST
 {
 public:
     std::unique_ptr<BaseAST> stmt;
-    void Dump(std::string& strOriginal) const override
+    void Dump(std::string &strOriginal) const override
     {
         strOriginal += "%entry: ";
         strOriginal += "\n";
         stmt->Dump(strOriginal);
-        strOriginal += "\n";
     }
 };
 
 class StmtAST : public BaseAST
 {
 public:
-    int Number;
-    void Dump(std::string& strOriginal) const override
+    std::unique_ptr<BaseAST> exp;
+    void Dump(std::string &strOriginal) const override
     {
+        // 最后一个level的寄存器
+        std::string lastLevelRegister = exp->ReversalDump(strOriginal);
         strOriginal += "  ret ";
-        strOriginal += std::to_string(Number);
+        strOriginal += lastLevelRegister;
+        strOriginal += "\n";
+    }
+};
+
+class ExpAST : public BaseAST
+{
+public:
+    std::unique_ptr<BaseAST> unaryExp;
+    void Dump(std::string &strOriginal) const override {}
+    // exp所用的寄存器或者一个number
+    std::string ReversalDump(std::string &strOriginal) override
+    {
+        return unaryExp->ReversalDump(strOriginal);
+    }
+};
+
+class PrimaryExpAST : public BaseAST
+{
+public:
+    std::unique_ptr<BaseAST> exp;
+    std::unique_ptr<BaseAST> Number;
+    std::string selfMinorType;
+    void Dump(std::string &strOriginal) const override {}
+    std::string ReversalDump(std::string &strOriginal) override
+    {
+        // exp
+        if (selfMinorType[0] == '0')
+        {
+            return exp->ReversalDump(strOriginal);
+        }
+        // number
+        else
+        {
+            return Number->ReversalDump(strOriginal);
+        }
+    }
+};
+
+class NumberAST : public BaseAST
+{
+public:
+    int Number;
+    void Dump(std::string &strOriginal) const override {}
+    // 对于一个number，直接返回它的大小
+    std::string ReversalDump(std::string &strOriginal) override
+    {
+        return std::to_string(Number);
+    }
+};
+
+class UnaryExpAST : public BaseAST
+{
+public:
+    std::unique_ptr<BaseAST> primaryExp;
+    std::unique_ptr<BaseAST> unaryOp;
+    std::unique_ptr<BaseAST> unaryExp;
+    std::string selfMinorType;
+    void Dump(std::string &strOriginal) const override {}
+    // 返回一个数字或者寄存器标号
+    std::string ReversalDump(std::string &strOriginal) override
+    {
+        // PrimaryExp
+        if (selfMinorType[0] == '0')
+        {
+            return primaryExp->ReversalDump(strOriginal);
+        }
+        // UnaryOp UnaryExp
+        else
+        {
+            std::string currOperator = unaryOp->ReversalDump(strOriginal);
+            std::string lastLevelRegister = unaryExp->ReversalDump(strOriginal);
+            // it is a number
+            if (lastLevelRegister[0] != '%')
+            {
+                if (currOperator[0] == '-')
+                {
+                    std::string tempStr = "  %0 = sub 0, ";
+                    tempStr += lastLevelRegister;
+                    tempStr += "\n";
+                    strOriginal += tempStr;
+                }
+                else if (currOperator[0] == '!')
+                {
+                    std::string tempStr = "  %0 = eq ";
+                    tempStr += lastLevelRegister;
+                    tempStr += ", 0\n";
+                    strOriginal += tempStr;
+                }
+                return "%0";
+            }
+            // it is a register
+            else
+            {
+                std::string tempLastLevelRegister = lastLevelRegister;
+                tempLastLevelRegister.erase(0, 1);
+                int currRegisterNum = std::atoi(tempLastLevelRegister.c_str()) + 1;
+                std::string currRegister = "%" + std::to_string(currRegisterNum);
+                if (currOperator[0] == '-')
+                {
+                    std::string tempStr = "  ";
+                    tempStr += currRegister;
+                    tempStr += " = sub 0, ";
+                    tempStr += lastLevelRegister;
+                    tempStr += "\n";
+                    strOriginal += tempStr;
+                }
+                else if (currOperator[0] == '!')
+                {
+                    std::string tempStr = currRegister;
+                    tempStr += " = eq ";
+                    tempStr += lastLevelRegister;
+                    tempStr += ", 0\n";
+                    strOriginal += tempStr;
+                }
+                else if (currOperator[0] == '+')
+                {
+                    return lastLevelRegister;
+                }
+                return currRegister;
+            }
+        }
+    }
+};
+
+class UnaryOpAST : public BaseAST
+{
+public:
+    std::string unaryOp;
+    void Dump(std::string &strOriginal) const override {}
+    // 返回符号
+    std::string ReversalDump(std::string &strOriginal) override
+    {
+        registerCnt += 1;
+        return unaryOp;
     }
 };
