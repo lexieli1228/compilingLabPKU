75f06b0ca83afa5c4ea4c82b1cf570c8ccea843f
lexieli1228 <84088099+lexieli1228@users.noreply.github.com>
2022-12-11 00:36:11 +0800

    Mod: focusing on front only
    commented out the koopa-riscv conversion in main function, added a variable for counting the total amount of registers assigned

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ using namespace std;
 
 extern FILE *yyin;
 extern FILE *yyout;
+
+// 记录当前寄存器使用个数
+int registerCnt = 0;
+
 extern int yyparse(unique_ptr<BaseAST> &ast);
 
 void Visit(const koopa_raw_program_t &program);
@@ -155,35 +159,35 @@ int main(int argc, const char *argv[])
   // dump AST into string
   ast->Dump(strOrigin);
 
-  // 转化成内存形式
-  // 解析字符串 str, 得到 Koopa IR 程序
-  koopa_program_t program;
-  koopa_error_code_t ret = koopa_parse_from_string(strOrigin.c_str(), &program);
-  assert(ret == KOOPA_EC_SUCCESS);  // 确保解析时没有出错
-  // 创建一个 raw program builder, 用来构建 raw program
-  koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
-  // 将 Koopa IR 程序转换为 raw program
-  koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
-  // 释放 Koopa IR 程序占用的内存
-  koopa_delete_program(program);
+  // // 转化成内存形式
+  // // 解析字符串 str, 得到 Koopa IR 程序
+  // koopa_program_t program;
+  // koopa_error_code_t ret = koopa_parse_from_string(strOrigin.c_str(), &program);
+  // assert(ret == KOOPA_EC_SUCCESS);  // 确保解析时没有出错
+  // // 创建一个 raw program builder, 用来构建 raw program
+  // koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+  // // 将 Koopa IR 程序转换为 raw program
+  // koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+  // // 释放 Koopa IR 程序占用的内存
+  // koopa_delete_program(program);
   
-  // get output
-  // lv1, directly output koopa program
+  // // get output
+  // // lv1, directly output koopa program
   if (mode[1] == 'k')
   {
     cout << strOrigin;
   }
-  // lv2, generate riscv output
-  // 处理 raw program
-  else if (mode[1] == 'r') 
-  {
-    Visit(raw);
-  }
+  // // lv2, generate riscv output
+  // // 处理 raw program
+  // else if (mode[1] == 'r') 
+  // {
+  //   Visit(raw);
+  // }
 
-  // 处理完成, 释放 raw program builder 占用的内存
-  // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
-  // 所以不要在 raw program 处理完毕之前释放 builder
-  koopa_delete_raw_program_builder(builder);
+  // // 处理完成, 释放 raw program builder 占用的内存
+  // // 注意, raw program 中所有的指针指向的内存均为 raw program builder 的内存
+  // // 所以不要在 raw program 处理完毕之前释放 builder
+  // koopa_delete_raw_program_builder(builder);
 
   return 0;
 }
